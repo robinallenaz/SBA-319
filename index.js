46e8f955e03e97c533ebcc77e48970febfd94e91
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-05 20:12:47 -0700

    Updated .gitignore

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+//Create GET routes for all data that should be exposed to the client, using appropriate query commands to retrieve the data from the database.
+app.get('/users', (req, res) => {
+    
+})
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 }); 
