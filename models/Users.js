6f5981f18adfdcf8d78ffd5d9a83d7ff07b9c549
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-05 20:33:51 -0700

    Created .env

---
+++ models/Users.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const users = require("../models/Users.js");
+const error = require("../myapp/middleware.js");
+
+// BASE PATH FOR THIS ROUTER IS: /api/users
+
+// Creating a simple GET route for individual users
+
+router.get("/", (req, res) => {
+  const links = [
+    {
+      href: "users/:id",
+      rel: ":id",
+      type: "GET",
+    },
+  ];
+
+  res.json({ users, links });
+});
+
+// Creating a simple GET route for individual users,
+// using a route parameter for the unique id.
+
+router.get("/:id", (req, res, next) => {
+  const user = users.find((u) => u.id == req.params.id);
+  const links = [
+    {
+      href: `/${req.params.id}`,
+      rel: "",
+      type: "PATCH",
+    },
+    {
+      href: `/${req.params.id}`,
+      rel: "",
+      type: "DELETE",
+    },
+  ];
